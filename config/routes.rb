1397688fbf45a1108c877285ca3a0ae0d235db21
sven-k <sven-k@738a3cfe-f54d-0410-852a-ebac1faf349d>
2009-09-20 11:54:19 +0000

    Compatibility with Redmine r2809
    git-svn-id: file:///home/sven/rmc_backup/svn_neu/trunk@268 738a3cfe-f54d-0410-852a-ebac1faf349d

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+ActionController::Routing::Routes.draw do |map|
+  map.connect "mylyn/issue/:id", :controller => "MylynConnector::Issues", :action => "show", :conditions => {:method => :get}
+  map.connect "mylyn/projects", :controller => "MylynConnector::Projects", :action => "all", :conditions => {:method => :get}
+  map.connect "mylyn/priorities", :controller => "MylynConnector::Priorities", :action => "all", :conditions => {:method => :get}
+  map.connect "mylyn/issuestatus", :controller => "MylynConnector::IssueStatus", :action => "all", :conditions => {:method => :get}
+  map.connect "mylyn/version", :controller => "MylynConnector::Information", :action => "version", :conditions => {:method => :get}
+  map.connect "mylyn/:project_id/search", :controller => "MylynConnector::Issues", :action => "query"
+  map.connect "mylyn/:project_id/updatedsince", :controller => "MylynConnector::Issues", :action => "updated_since"
+end
