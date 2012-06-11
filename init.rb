d8b1164a801912818cae94f05dbb194ec7fffbee
Daniel Munn <dan.munn@munnster.co.uk>
2012-06-11 11:50:14 +0100

    Backwards compatibility for Redmine 1.x

--- init.rb
+++ init.rb
@@ -9,11 +9,19 @@ Redmine::Plugin.register :redmine_mylyn_connector do
   version MylynConnector::Version
 end
 
-Redmine::Plugin.find(:redmine_mylyn_connector).requires_redmine(:version_or_higher=>'2.0.0')
+Redmine::Plugin.find(:redmine_mylyn_connector).requires_redmine(:version_or_higher=>'1.3.0')
 
 require_dependency 'mylyn_connector/hooks/controller_issues_edit_after_save'
-
-#require 'mylyn_connector/patches/custom_value_patch'
-Rails.application.config.to_prepare do
-  CustomValue.send(:include, MylynConnector::Patches::CustomValuePatch) unless CustomValue.included_modules.include?(MylynConnector::Patches::CustomValuePatch)
+require 'mylyn_connector/patches/custom_value_patch'
+if Gem::Version.new("3.0") > Gem::Version.new(Rails.version) then
+  #Redmine 1.x
+  require 'dispatcher'
+  Dispatcher.to_prepare do
+    CustomValue.send(:include, MylynConnector::Patches::CustomValuePatch) unless CustomValue.included_modules.include?(MylynConnector::Patches::CustomValuePatch)
+  end
+else
+  #Redmine 2.x
+  Rails.application.config.to_prepare do
+    CustomValue.send(:include, MylynConnector::Patches::CustomValuePatch) unless CustomValue.included_modules.include?(MylynConnector::Patches::CustomValuePatch)
+  end
 end
